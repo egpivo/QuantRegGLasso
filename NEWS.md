512ca40b79762b32e0bf24ece1ba36f7051b9f2d
Joseph (Wen-Ting) Wang <egpivo@gmail.com>
2024-01-06 17:25:39 +0800

    [Enhancement] Add example code to the main function (#3)
    * CHG: refine the function orthgonize_bspline by enhancing code quality and code coverage, and fixing typos
    
    * CHG: add a missing manual
    
    * CHG: add a one-dim example; enhance the unit tests
    
    * CHG: add the example plotsa
    
    * CHG: update the changelog; increment the version number

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,11 @@
+## QuantRegGLasso 0.2.0 (Release Date: 2024-01-06)
+### Overview 
+- Added one-dimensional example code in the main function `qrglasso`.
+- Improved code quality and increased code coverage for the helper function `orthogonize_bspline`.
+
+---
 ## QuantRegGLasso 0.1.0 (Release date: 2024-01-05)
 #### Overview 
-- Add a helper function to orthognize B-splines
+- Add a helper function to orthogonize B-splines
 
 ---
--- R/helper.R
+++ R/helper.R
@@ -1,110 +1,85 @@
 #' Orthogonalized B-splines
-#' @param knots  Array. The knots that define the spline.
+#' @param knots Array. The knots that define the spline.
 #' @param boundary_knots Array. The breakpoints that define the spline.
 #' @param degree Integer. The degree of the piecewise polynomial.
 #' @param predictors Array. The predictor variables with size p.
 #' @param is_approx Boolean. The default is `FALSE`.
-#' @return his function returns a \code{list} including:
-#' \itemize{
-#'  \item{bsplines}{Matrix Orthogonalized B-splines with dimenstion (p, length(knots) + degree + 1}
-#'  \item{z}{Predictors used in generation)}
-#' }
-#' 
+#' @return A list containing:
+#'   \item{bsplines}{Matrix of orthogonalized B-splines with dimension (p, length(knots) + degree + 1)}
+#'   \item{z}{Predictors used in generation}
 #' @export
 #' @examples
 #' p <- 30
 #' total_knots <- 10
 #' degree <- 3
 #' boundaries <- c(0, 1)
-#' x <- seq(from = 0, to = 1,length.out = total_knots)
+#' x <- seq(from = 0, to = 1, length.out = total_knots)
 #' knots <- x[2:(total_knots - 1)]
 #' predictors <- runif(p, min = 0, max = 1)
-#' bsplines <- orthgonize_bspline(knots, boundaries, degree, predictors)
-#' # Plot the first 5 B-splints
+#' bsplines <- orthogonize_bspline(knots, boundaries, degree, predictors)
+#' # Plot the first 5 B-splines
 #' index <- order(bsplines$z)
-#' originalPar <- par(no.readonly = TRUE)
-#' par(mfrow=c(1, 5))
-#' for(i in 1:5)
-#'   plot(bsplines$z[index], bsplines$bsplines[index, i], main=i, type="l")
-#' par(originalPar)
+#' original_par <- par(no.readonly = TRUE)
+#' par(mfrow = c(1, 5))
+#' for (i in 1:5)
+#'   plot(bsplines$z[index], bsplines$bsplines[index, i], main = i, type = "l")
+#' par(original_par)
 #' 
-orthgonize_bspline <- function(
-    knots, boundary_knots, degree, predictors = NULL, is_approx = FALSE
-  ) {
-    if (is_approx) {
-      if (is.null(predictors)) {
-        nrep <- 1e6
-        x <- 1:nrep
-        x <- x / nrep
-      } else if (length(predictors) < 1e6) {
-        nrep <- 1e6
-        x <-
-          sort(c(predictors, runif(
-            1e6 - length(predictors), min = 0, max = 1
-          )))
-      } else {
-        nrep <- length(predictors)
-        x <- predictors
-      }
-    } else{
-      if (is.null(predictors)) {
-        nrep <- 1e4
-        x <- (1:nrep) / nrep
-      } else{
-        nrep <- length(predictors)
-        x <- predictors
-      }
-    }
-    
-    bs0 <-
-      bs(
-        x,
-        knots = knots,
-        Boundary.knots = boundary_knots,
-        degree = degree,
-        intercept = TRUE
-      )
 
-    LL <- dim(bs0)[2]
-    bs1 <- matrix(0, nrow = nrep, ncol = LL)
-    a0 <- diag(LL)
+orthogonize_bspline <- function(
+    knots, boundary_knots, degree, predictors = NULL, is_approx = FALSE
+) {
+  # Determine the number of replications
+  nrep <- if (is_approx) {
+    if (is.null(predictors)) 1e6 else min(1e6, length(predictors))
+  } else {
+    if (is.null(predictors)) 1e4 else length(predictors)
+  }
+  
+  # Generate predictors
+  x <- if (is.null(predictors)) seq(0, 1, length.out = nrep) else
+    if (length(predictors) < nrep) sort(c(predictors, runif(nrep - length(predictors), min = 0, max = 1))) else
+      predictors
+  
+  # Generate B-spline basis
+  bs0 <- bs(x, knots = knots, Boundary.knots = boundary_knots, degree = degree, intercept = TRUE)
+  num_basis_functions <- dim(bs0)[2]
+  bsplines_matrix <- matrix(0, nrow = nrep, ncol = num_basis_functions)
+  a_matrix <- diag(num_basis_functions)
+  
+  sqLL <- 1 / sqrt(num_basis_functions)
+  bsplines_matrix[, 1] <- sqLL
+  bsplines_matrix[, 2] <- sqrt(12) * sqLL * (x - 0.5)
+  
+  # Main loop for orthogonalization
+  for (i in 3:num_basis_functions) {
+    bsplines_matrix[, i] <- bs0[, i - 1]
+    current_basis <- bsplines_matrix[, i]
     
-    sqLL <- 1 / sqrt(LL)
-    bs1[, 1] <- sqLL
-    bs1[, 2] <- sqrt(12) * sqLL * (x - 0.5)
-    
-    for (ii in 3:LL)
-      bs1[, ii] <- bs0[, (ii - 1)]
-    
-    for (jj in 3:LL) {
-      ee <- as.numeric(bs1[, jj])
-      
-      for (ii in 1:(jj - 1))
-        a0[jj, ii] <- -LL * (mean(ee * bs1[, ii]))
-      
-      for (ii in 1:(jj - 1))
-        ee <- ee + a0[jj, ii] * as.numeric(bs1[, ii])
-      
-      nee <- sqrt(mean(ee ** 2))
-      bs1[, jj] <- ee / nee * sqLL
-      a0[jj,] <- a0[jj,] * sqLL / nee
+    # Orthogonalization
+    for (j in 1:(i - 1)) {
+      a_matrix[i, j] <- -num_basis_functions * mean(current_basis * bsplines_matrix[, j])
+      current_basis <- current_basis + a_matrix[i, j] * bsplines_matrix[, j]
     }
     
-    if (is.null(predictors) || is_approx == FALSE) {
-      bsplines <- bs1
-      z <- x
-    } else {
-      idx <-
-        sapply(1:length(predictors), function(i)
-          which(x == predictors[i])[1])
-      bsplines <- bs1[unlist(idx),]
-      z <- predictors
-    }
-    
-    return(
-      list(
-        bsplines = matrix(bsplines, ncol = length(knots) + degree + 1),
-        z = z
-      )
-    )
+    # Normalize and update basis
+    nee <- sqrt(mean(current_basis^2))
+    bsplines_matrix[, i] <- current_basis / nee * sqLL
+    a_matrix[i, ] <- a_matrix[i, ] * sqLL / nee
+  }
+  
+  # Prepare output
+  if (is.null(predictors) || !is_approx) {
+    bsplines_output <- bsplines_matrix
+    z_values <- x
+  } else {
+    idx <- sapply(predictors, function(p) which(x == p)[1])
+    bsplines_output <- bsplines_matrix[idx, ]
+    z_values <- predictors
+  }
+  
+  return(list(
+    bsplines = matrix(bsplines_output, ncol = length(knots) + degree + 1),
+    z = z_values
+  ))
 }
